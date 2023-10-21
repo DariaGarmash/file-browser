88ab902fe0e2521f84a697dd22d44aebf1ecb112
Daria Garmash <daria@penni.io>
2023-10-22 01:18:06 +0200

    cleanup

--- src/tests/NodeIcon.test.tsx
+++ src/tests/NodeIcon.test.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../store/store";
 import NodeIcon, { NodeIconProps } from "../components/NodeIcon";
 
 const getNodeIconElement = (args: NodeIconProps): Element => {
 	const {container, debug} = render(<Provider store={store}><NodeIcon {...args}/></Provider>)
-	debug()
 	return container.getElementsByClassName('node-icon')[0]
 }
 
